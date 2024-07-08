84069dc047928c06a55a27b7a980dfac6e63b80f
Rovin Singh <rovin.singh.it@ghrce.raisoni.net>
2024-07-08 18:13:20 +0530

    Final commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,7 @@ import { Toaster } from '@/components/ui/toaster';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'True Feedback',
+  title: 'Anon Messages',
   description: 'Real feedback from real people.',
 };
 
