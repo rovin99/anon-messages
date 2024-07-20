96edd8aab03363b5a198d173c221d78d9ad020e8
Rovin Singh <rovin.singh.it@ghrce.raisoni.net>
2024-07-20 14:53:49 +0530

    Fixed typo in suggest message

--- src/app/api/suggest-messages/route.ts
+++ src/app/api/suggest-messages/route.ts
@@ -1,5 +1,5 @@
-import  Anthropic  from '@anthropic-ai/sdk';
-import { NextResponse } from 'next/server';
+import Anthropic from '@anthropic-ai/sdk';
+import { NextRequest, NextResponse } from 'next/server';
 
 const anthropic = new Anthropic({
   apiKey: process.env.ANTHROPIC_API_KEY,
@@ -7,7 +7,7 @@ const anthropic = new Anthropic({
 
 export const runtime = 'edge';
 
-export async function POST(req: Request) {
+export async function POST(req: NextRequest) {
   try {
     const prompt =
       "Create a list of three open-ended and engaging questions formatted as a single string. Each question should be separated by '||'. These questions are for an anonymous social messaging platform, like Qooh.me, and should be suitable for a diverse audience. Avoid personal or sensitive topics, focusing instead on universal themes that encourage friendly interaction. For example, your output should be structured like this: 'What's a hobby you've recently started?||If you could have dinner with any historical figure, who would it be?||What's a simple thing that makes you happy?'. Ensure the questions are intriguing, foster curiosity, and contribute to a positive and welcoming conversational environment.";
@@ -15,22 +15,19 @@ export async function POST(req: Request) {
     const response = await anthropic.completions.create({
       model: 'claude-3-sonnet-20240229',
       max_tokens_to_sample: 400,
-      stream: true,
       prompt,
     });
 
-    
-
-    return response;
+    return NextResponse.json({ questions: response.completion });
   } catch (error) {
     if (error instanceof Anthropic.APIError) {
       // Anthropic API error handling
       const { name, status, headers, message } = error;
-      return NextResponse.json({ name, status, headers, message }, { status });
+      return NextResponse.json({ name, status, headers, message }, { status: status || 500 });
     } else {
       // General error handling
       console.error('An unexpected error occurred:', error);
-      throw error;
+      return NextResponse.json({ message: 'An unexpected error occurred' }, { status: 500 });
     }
   }
 }
